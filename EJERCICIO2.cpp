1b354b80769ad9c35a2eb21a931c55e866e7cd31
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-11 21:39:11 -0400

    Add files via upload

---
+++ EJERCICIO2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	short int numero_entero_p;
+	int numero_entero;
+	float numero_decimal;
+	double numero_decimal_g;
+	//Asignando valores limite de los tipos de dato
+	numero_entero_p=32767;//valor limite de los tipo de dato 
+	cout <<"numero entero pequeño: "<<numero_entero<<endl;	
+	numero_entero_p=65535; //valor limite del tipo de dato
+	cout <<"numero entero pequeño: "<<numero_entero_p<<endl;
+	return 0;
+}
+
