d0ab40837e30b8b1369e1d36581e1ad0f036f988
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-11 21:52:56 -0400

    Update EJERCICIO2.cpp

--- EJERCICIO2.cpp
+++ EJERCICIO2.cpp
@@ -5,13 +5,23 @@ int main(int argc, char *argv[])
 {
 	short int numero_entero_p;
 	int numero_entero;
+	unsing int numero_entero_s;
 	float numero_decimal;
 	double numero_decimal_g;
 	//Asignando valores limite de los tipos de dato
 	numero_entero_p=32767;//valor limite de los tipo de dato 
-	cout <<"numero entero peque�o: "<<numero_entero<<endl;	
+	cout <<"numero entero pequeño: "<<numero_entero<<endl;	
 	numero_entero_p=65535; //valor limite del tipo de dato
-	cout <<"numero entero peque�o: "<<numero_entero_p<<endl;
+	cout <<"numero entero pequeño: "<<numero_entero_p<<endl;
+	
+	numero_entero=32767;//valor limite de los tipo de dato 
+	cout <<"numero entero pequeño : "<<numero_entero<<endl;	
+	numero_entero_s=65535; //valor limite del tipo de dato
+	cout <<"numero entero pequeño positivo: "<<numero_entero_p<<endl;
+	//asignar los valores maximos para unsingned int y int 
+	numero_entero=2147483647;
+	cout <<"numero entero:"<<numero_entero>>endl;
+	numero_entero_s=quinto numero_p
 	return 0;
 }
 
